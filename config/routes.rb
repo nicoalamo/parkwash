624099bded4f8509f6b29e26a3343badf9876e73
nicoalamo <nicoalamo@gmail.com>
2018-09-25 21:58:18 -0300

    Washers devise controller and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'pages/index'
-  root 'pages#index'
+
+  devise_for :washers
+  # devise_for :users
+
+  root to: 'pages#index'
+
+  devise_for :users, controllers: {
+          registrations: 'users/registrations'
+        }
+
+
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
